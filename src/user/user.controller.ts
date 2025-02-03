a613e5868a5a0f4503ad77101d8427400d5bff2e
Allan <allanhsz@outlook.com>
2025-02-03 19:32:19 -0300

    feat: add create, get, delete and edit in user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.create(userData);
+  }
+
+  @Get(':id')
+  async getUser(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<UserModel | null> {
+    return this.userService.get({ id });
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Body() userData: Prisma.UserUpdateInput,
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<UserModel> {
+    return this.userService.update({
+      where: { id },
+      data: userData,
+    });
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.delete({ id });
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,36 @@
+import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+
+import { PrismaService } from 'src/database/prisma.service';
+
+type UpdateUserParams = {
+  where: Prisma.UserWhereUniqueInput;
+  data: Prisma.UserUpdateInput;
+};
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async get(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: userWhereUniqueInput });
+  }
+
+  async create(user: Prisma.UserCreateInput) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const password = await bcrypt.hash(user.password, 10);
+    return this.prisma.user.create({ data: { ...user, password } });
+  }
+
+  async update(params: UpdateUserParams): Promise<User> {
+    const { data, where } = params;
+    return this.prisma.user.update({ data, where });
+  }
+
+  async delete(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({ where });
+  }
+}
