cbccc2d354ae20f746bce5785dad8162de5e0638
Allan <allanhsz@outlook.com>
2025-02-03 21:34:12 -0300

    feat: add signin endpoint to get acess_token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  async sigIn(@Body() user: Prisma.UserCreateInput) {
+    return this.authService.signIn(user);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signIn(params: Prisma.UserCreateInput): Promise<{
+    access_token: string;
+  }> {
+    const user = await this.userService.get({ email: params.email });
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const passwordMatch = await bcrypt.compare(params.password, user.password);
+    if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
+
+    const payload = { sub: user.id };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
